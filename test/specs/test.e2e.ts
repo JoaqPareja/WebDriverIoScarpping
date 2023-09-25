cc5f8179bafcbc6b1d64884546586a1f510c371a
Joaquin Pareja <joaq.pareja@gmail.com>
2023-09-25 17:08:45 -0300

    adding necessary comments

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,10 +1,10 @@
-import { expect } from '@wdio/globals'
+import { expect } from '@wdio/globals' //This imports the expect function from the WebDriverIO's globals module, which is commonly used for assertion and expectation testing.
 import Homepage from '../pageobjects/homepage.page.ts'
-import Articulos from '../pageobjects/articulos.page.ts'
-import { writeFile } from 'fs/promises';
+import Articulos from '../pageobjects/articulos.page.ts' //These lines import the Homepage and Articulos page objects from the respective TypeScript files located in the pageobjects directory.
+import { writeFile } from 'fs/promises'; //This imports the writeFile function from the Node.js fs/promises module, which is used for asynchronous file writing.
 
-describe('Get Mercado libre information', () => {
-    before(async()=>{
+describe('Get Mercado libre information', () => { //This defines a test suite with the description 'Get Mercado libre information'. This suite contains a set of test cases.
+    before(async()=>{ //This is a setup hook that runs before any of the test cases within the suite. It opens the Homepage page.
         await Homepage.open()
     })
     it('Get Camisetas information', async () => {
@@ -14,7 +14,7 @@ describe('Get Mercado libre information', () => {
         await Homepage.clickSearchBtn();
         await expect(browser).toHaveUrlContaining('camiseta')
         await Articulos.getArticlesInformationFromPages()
-        browser.pause(10000);
+        // browser.pause(10000); this can pause the browser in case we want to pause the execution of the code
     })
     it('get Information', async()=>{
         await writeFile('./file.txt', Articulos.joinArrs);
